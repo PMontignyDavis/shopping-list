f630ca882223a7dd31080f8d8a620402819d17e5
Pamela Montigny-Davis <pmontignydavis@cnm.edu>
2019-09-22 19:10:06 -0600

    added index.html

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-window.unload = function(){
+window.onload = function () {
 	initShoppingList();
 };
 
-function initShoppingList(){
+function initShoppingList () {
 	let form = document.getElementById("item-form");
 
 	form.addEventListener("submit",(event) => {
@@ -10,29 +10,51 @@ function initShoppingList(){
 	});
 }
 
-function handleItemForm(event, formRef){
-	if(event.preventDefault){
-		event.preventDefault();
+function handleItemForm (event, formRef) {
+	if(event.preventDefault) {
+		event.preventDefault ();
 	}
 
-	addItemToShoppingList();
+	addItemToShoppingList ();
+	formRef.reset ();
 
 	return false;
 }
 
-function addItemToShoppingList (){
+function addItemToShoppingList () {
 	let itemName = document.getElementById("item-name");
 	let itemAmount = document.getElementById("item-amount");
+	let id = getRandomInt (0, 10000000);
 
-	let itemHtml = createListItemHtml(itemName.value, itemAmount.value);
+	let itemHtml = createListItemHtml(itemName.value, itemAmount.value, id);
 	console.log("Item HTML: ", itemHtml);
-	let itemListRef = document.getElementById("shopping-List");
+	let itemListRef = document.getElementById("shopping-list");
 	itemListRef.insertAdjacentHTML("afterend", itemHtml);
+
+setDeleteButtonEvent (id);
 }
 
-function createListItemHtml (itemName, itemAmount){
-	return `<li>
+function setDeleteButtonEvent (id) {
+	let deleteButton = document.getElementById ("button" + id);
+	deleteButton.addEventListener("click", () => {
+		removeListItem (id);
+	});
+}
+
+function createListItemHtml (itemName, itemAmount, id) {
+	return `<li id="item${id}">
 					${itemName} - ${itemAmount}
-					<button type="button">Delete Item</button>
-				</li>`
+					<button id="button${id}" type="button">Delete Item</button>
+			  </li>`;
 	}
+
+function removeListItem (id) {
+	let listItem = document.getElementById ("item" +id);
+	listItem.parentNode.removeChild (listItem);
+}
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
